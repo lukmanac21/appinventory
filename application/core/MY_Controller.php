64426843a1ba648e6d183a721111844baf0f06cb
Gunawan <69330442+gunawansutrisno@users.noreply.github.com>
2020-10-13 12:55:48 +0700

    -
    -

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -174,7 +174,7 @@ class MY_Controller extends CI_Controller {
                 "sub_menu" => "pengguna_sub"
             ),
             "laporan" =>
-            array("class" => "glyphicon glyphicon-user text-purple",
+            array("class" => "glyphicon glyphicon-stats text-purple",
                 "desc" => "Laporan",
                 "main_url" => $this->base_url_site . "",
                 "sub_menu" => "laporan_sub"
@@ -268,7 +268,7 @@ class MY_Controller extends CI_Controller {
             "laporan_sub" => array(
                 "add_laporan" => array(
                     "class" => "glyphicon glyphicon-edit text-purple",
-                    "url" => "laporam/",
+                    "url" => "laporan/",
                     "desc" => "Laporan Data Kain",
                 ),
                
